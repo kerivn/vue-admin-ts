b25e08185b185d3a1f8c6057ce61fd86fe8bdf50
kervin <kervin_s@outlook.com>
2021-06-15 18:13:36 +0800

    feat: login init

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,20 @@
  * @Description: 入口ts文件
  * @FilePath: \vue-admin-ts\src\main.ts
  */
-import { createApp } from "vue";
+import Vue, { createApp } from "vue";
 import App from "./App.vue";
-import '@/styles/index.scss'
+import "@/styles/index.scss";
 import router from "./router";
 import store from "./store";
 import ElementPlus from "element-plus";
 import "element-plus/lib/theme-chalk/index.css";
 // import '@/styles/index.scss'
 const app = createApp(App);
-app.use(store)
-.use(router)
-.use(ElementPlus)
-.mount("#app");
+const win: any = window; //
+if (process.env.NODE_ENV === "development") {
+  if ("__VUE_DEVTOOLS_GLOBAL_HOOK__" in win) {
+    // 这里__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue赋值一个createApp实例
+    win.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app;
+  }
+}
+app.use(store).use(router).use(ElementPlus).mount("#app");
--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -17,17 +17,40 @@
       >
         <el-form-item prop="username">
           <el-input
+            name="username"
+            type="text"
+            ref="userNameRef"
+            v-model="loginForm.username"
+            tabindex="1"
+            autocomplete="on"
             placeholder="请输入用户名！"
             prefix-icon="el-icon-user"
           ></el-input>
         </el-form-item>
-        <el-form-item prop="password">
-          <el-input
-            placeholder="请输入登录密码！"
-            prefix-icon="el-icon-lock"
-          ></el-input>
-        </el-form-item>
-        <el-form-item prop="isChecked">
+        <el-tooltip
+          v-model="capsTooltip"
+          content="Caps lock is On"
+          placement="right"
+          manual
+        >
+          <el-form-item prop="password">
+            <el-input
+              class="password"
+              ref="passwordRef"
+              :type="passwordType"
+              v-model="loginForm.password"
+              placeholder="请输入登录密码！"
+              prefix-icon="el-icon-lock"
+              name="password"
+              tabindex="2"
+              autocomplete="on"
+              @keyup="checkCapslock"
+              @blur="capsTooltip = false"
+              @keyup.enter="handleLogin"
+            ></el-input>
+          </el-form-item>
+        </el-tooltip>
+        <el-form-item prop="isChecked" class="agreement">
           <el-checkbox v-model="isChecked"> </el-checkbox
           ><span
             >&nbsp;我已阅读并同意<el-link
@@ -50,14 +73,35 @@
         </el-form-item>
         <div class="tips">
           <span>默认用户名 ：admin1</span>
-          <span>默认密码 ： root</span><br>
+          <span>默认密码 ： root</span><br />
           <span>默认用户名 ：admin2</span>
           <span>默认密码 ： root</span>
         </div>
       </el-form>
-        <el-divider content-position="left">第三方登录</el-divider>
+      <el-divider content-position="left">第三方登录</el-divider>
       <div class="footer-box">
-      
+        <div class="signup-container">
+          <span class="qq-svg-container">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-QQ" />
+            </svg>
+          </span>
+          <span class="wx-svg-container">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-wechat" />
+            </svg>
+          </span>
+          <span class="al-svg-container">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-icon_alipay" />
+            </svg>
+          </span>
+          <span class="weibo-svg-container">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#icon-weibo" />
+            </svg>
+          </span>
+        </div>
       </div>
     </div>
     <div class="right">
@@ -66,18 +110,19 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref, reactive, toRefs } from "vue";
+import { defineComponent, ref, reactive, toRefs, nextTick } from "vue";
 
 export default defineComponent({
+  name: "login",
   setup() {
     const loginFormRef = ref(null);
     const userNameRef = ref(null);
     const passwordRef = ref(null);
-    const isChecked: boolean = false;
+    const isChecked = ref(false);
     const state = reactive({
       loginForm: {
-        username: "admin",
-        password: "111111",
+        username: "",
+        password: "",
       },
       loginRules: {
         username: [{ validator: userNameRef, trigger: "blur" }],
@@ -91,12 +136,31 @@ export default defineComponent({
       redirect: "",
       otherQuery: {},
     });
+    const methods = reactive({
+      checkCapslock: (e: KeyboardEvent) => {
+        if (e.key === "CapsLock") state.capsTooltip = !state.capsTooltip;
+      },
+      handleLogin: () => {
+        console.log(213);
+      },
+      showPwd: () => {
+        if (state.passwordType === "password") {
+          state.passwordType = "";
+        } else {
+          state.passwordType = "password";
+        }
+        nextTick(() => {
+          (passwordRef.value as any).focus();
+        });
+      },
+    });
 
     return {
       userNameRef,
       passwordRef,
       loginFormRef,
       ...toRefs(state),
+      ...toRefs(methods),
     };
   },
 });
@@ -109,11 +173,9 @@ export default defineComponent({
   align-items: center;
   justify-content: space-around;
   background: linear-gradient(225deg, #1493fa, #01c6fa);
-  // background-color: $loginBg;
   .left {
     width: 478px;
     height: 550px;
-    // background: #f5f5f5;
     background-color: #fff;
     padding: 48px;
     .title-box {
@@ -140,15 +202,35 @@ export default defineComponent({
     }
     .login-form {
       margin-top: 40px;
+      .password::after {
+        content: "";
+        font-family: element-icons !important;
+        position: absolute;
+        right: 10px;
+        font-size: 16px;
+        color: #ccc;
+        cursor: pointer;
+        user-select: none;
+      }
+
       .tips {
         font-size: 14px;
         color: #1493fa;
         margin-bottom: 10px;
-
         span {
           &:nth-of-type(odd) {
             margin-right: 16px;
           }
+        }
+      }
+    }
+    .footer-box {
+      .signup-container {
+        display: flex;
+        svg.icon {
+          margin-right: 25px;
+          width: 40px;
+          height: 40px;
         }
       }
     }
